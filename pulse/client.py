d403385758a9e4d5e95640936d675a71f1a00591
jnsougata <jnsougata@gmail.com>
2025-04-22 15:10:29 +0530

    Add htmx method to dynamically set script source in head

--- pulse/client.py
+++ pulse/client.py
@@ -23,9 +23,11 @@ class App(Starlette):
         self.head.append(HTMLElement("meta", self_enclosing=True, auto_id=False).set(charset="utf-8"))
         self.head.append(HTMLElement("meta", self_enclosing=True, auto_id=False)
                          .set(name="viewport", content="width=device-width, initial-scale=1.0"))
-        self.head.append(HTMLElement("script", auto_id=False).set(src="https://unpkg.com/htmx.org@2.0.4"))
         self.body = HTMLElement("body", auto_id=False)
         self.add_route(path, lambda _: HTMLResponse(str(self)), methods=methods)
+
+    def htmx(self, src: str = "https://unpkg.com/htmx.org@2.0.4"):
+        self.head.append(HTMLElement("script", auto_id=False).set(src=src))
 
     def link(self, rel: str, href: str, **attrs) -> HTMLElement:
         link = HTMLElement("link").set(rel=rel, href=href)
